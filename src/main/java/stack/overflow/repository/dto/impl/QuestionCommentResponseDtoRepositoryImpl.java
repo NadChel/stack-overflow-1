26cc4ba754fb499ecfee045c2b36259c664776e0
Sergey <nad.chel@mail.ru>
2023-06-05 20:29:31 +0300

    DESC не работает?

--- src/main/java/stack/overflow/repository/dto/impl/QuestionCommentResponseDtoRepositoryImpl.java
+++ src/main/java/stack/overflow/repository/dto/impl/QuestionCommentResponseDtoRepositoryImpl.java
@@ -33,16 +33,14 @@ public class QuestionCommentResponseDtoRepositoryImpl implements QuestionComment
 
     @Override
     public List<QuestionCommentResponseDto> getDtosWithoutSetOwner(PaginationParameters params) {
-        String sortingModifier = PaginationParametersProcessor.extractSortingModifier(params);
+        String sortingModifier = PaginationParametersProcessor.extractSortingModifier(params).trim();
         int offset = PaginationParametersProcessor.extractFirstResultIndex(params);
         int limit = PaginationParametersProcessor.extractMaxResults(params);
         return entityManager.createQuery("""
                 SELECT new stack.overflow.model.dto.response.QuestionCommentResponseDto (
                             qc.id, q.id, qc.createdDate, qc.modifiedDate, qc.text
                 ) FROM QuestionComment qc JOIN qc.question q
-                ORDER BY :sort
-                """, QuestionCommentResponseDto.class)
-                .setParameter("sort", sortingModifier)
+                ORDER BY qc.""" + sortingModifier, QuestionCommentResponseDto.class)
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
--- src/main/java/stack/overflow/util/PaginationParametersProcessor.java
+++ src/main/java/stack/overflow/util/PaginationParametersProcessor.java
@@ -2,9 +2,27 @@ package stack.overflow.util;
 
 import stack.overflow.model.pagination.PaginationParameters;
 
+import javax.persistence.Entity;
+
 public class PaginationParametersProcessor {
     public static String extractSortingModifier(PaginationParameters params) {
         return params.sortType().getQuery();
+    }
+
+    public static String extractSortingModifier(PaginationParameters params,
+                                                Class<?> entity) {
+        String nonadaptedQuery = extractSortingModifier(params);
+        String entityTableName = extractEntityTableName(entity);
+        String adaptedQuery = nonadaptedQuery.replaceFirst("(?=\\w)", entityTableName + ".");
+        return adaptedQuery;
+    }
+
+    private static String extractEntityTableName(Class<?> entity) {
+        if (entity.isAnnotationPresent(Entity.class)) {
+            return entity.getSimpleName();
+        } else {
+            throw new IllegalArgumentException(String.format("%s is not an @Entity", entity.getName()));
+        }
     }
 
     public static int extractFirstResultIndex(PaginationParameters params) {
